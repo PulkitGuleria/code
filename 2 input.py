e770fd6fab964fe9213942e2824ebb8c8e0fd004
PulkitGuleria <152914029+PulkitGuleria@users.noreply.github.com>
2023-12-15 22:41:46 +0530

    Add files via upload

---
+++ 2 input.py
@@ -0,0 +1,5 @@
+name = "pulkit"
+last = "guleria"
+print(name , last)
+name = input("what is your name?:")
+print("hello" + name)#press enter in terminal
---
+++ 3 conversion.py
@@ -0,0 +1,5 @@
+old_age = input("enter your old age : ")
+new_age = int(old_age) + 2 #int for conversion strint into integer
+print(new_age)
+print(float(18))
+# str() bool()
